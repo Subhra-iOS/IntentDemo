7cc11e8d516ca800231000016fff8372c3a00ea3
Subhra Roy <subhra.roy@e-arc.com>
2019-08-05 19:08:28 +0530

    Add imageVIew

--- app/src/main/java/com/example/intentdemo/MainActivity.kt
+++ app/src/main/java/com/example/intentdemo/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.intentdemo
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var placeHolderImgView : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        placeHolderImgView = findViewById(R.id.placeholderImgView) as ImageView
+
+        placeHolderImgView.setOnClickListener {
+
+        }
 
     }
 
